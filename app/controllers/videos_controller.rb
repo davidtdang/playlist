2f0a3bf2562d86cf5ce554ab8a28e6d9bb0b53c9
asell22 <alexanderjsell@gmail.com>
2015-02-25 12:22:17 -0800

    added models playlist and group

---
+++ app/controllers/videos_controller.rb
@@ -0,0 +1,31 @@
+class VideosController < ApplicationController
+
+  def index
+    videos = Video.all
+    render json: videos
+  end
+
+  def create
+    video = Video.new(video_params)
+    if video.save
+      render json: video
+    end
+  end
+
+  def destroy
+    video = Video.find(params[:id])
+    video.destroy
+    head :no_content
+  end
+
+  def update
+    video = Video.find(params[:id])
+    video.update(video_params)
+    render json: video
+  end
+
+  private
+  def video_params
+    params.require(:video).permit(:author, :title, :link, :uid)
+  end
+end
