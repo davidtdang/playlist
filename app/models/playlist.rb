41b1be0f5bd72c0f1e4da03c17ad6b5b13f059eb
David Dang <dtudang@gmail.com>
2015-02-27 10:31:41 -0800

    modified serializers and models

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,5 +1,5 @@
 class Playlist < ActiveRecord::Base
   belongs_to :user
   belongs_to :group
-  has_many :videos
+  has_many :videos, dependent: :destroy 
 end
--- app/serializers/video_serializer.rb
+++ app/serializers/video_serializer.rb
@@ -1,3 +1,4 @@
 class VideoSerializer < ActiveModel::Serializer
   attributes :id, :title, :description, :thumbnail, :videoId
+  belongs_to :playlist, include: true
 end
